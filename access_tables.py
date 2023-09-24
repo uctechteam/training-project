dda25f7111d2ab1b3f3482fcc5f8d419cfc67d28
unknown <abida@uchicago.edu>
2023-09-24 09:08:46 -0500

    forgot to commit changes to db after adding, deleting

--- access_tables.py
+++ access_tables.py
@@ -15,6 +15,16 @@ class StudentCourses:
                 end_time REAL NOT NULL
                 )
                 ''')
+        self.con.commit()
+
+    def close_connection(self):
+        ''' Closes the cursor and the connection to the database'''
+        self.cur.close()
+        self.con.close()
+
+    def print_table(self):
+        self.cur.execute("SELECT * FROM student_courses")
+        return self.cur.fetchall()
     
     def get_student_courses(self, student):
         '''Retrieve a list of courses associated with a student in the format
@@ -27,13 +37,15 @@ class StudentCourses:
         '''Delete all entries in the table for the given student'''
         delete_statement = "DELETE FROM student_courses WHERE student = '{student}' ".format(student=student)
         self.cur.execute(delete_statement)
+        self.con.commit()
     
     def delete_student_course(self, student, course):
         '''Delete all entries in the table for the given student'''
         delete_statement = '''
             DELETE FROM student_courses WHERE student = '{student}' AND course = '{course}'
             '''.format(student=student, course=course)
-        courses = self.cur.execute(delete_statement)
+        self.cur.execute(delete_statement)
+        self.con.commit()
     
     def add_student_course(self, student, course):
         ''' Add a new course for the student if there are no overlaps, 
@@ -60,29 +72,34 @@ class StudentCourses:
             student_courses
             VALUES (NULL,?,?,?,?) '''
         self.cur.execute(select_statement, params)
-        res = self.cur.execute("SELECT * FROM student_courses")
-        return self.cur.fetchall()
+        self.cur.execute("SELECT * FROM student_courses")
+        res = self.cur.fetchall()
+        self.con.commit()
+        return res
 
 def main():
     # initialize the studentcourses class
     studentcourses = StudentCourses()
+    # Uncomment the following code if first time running script
     # Add first entry for student 'Abida'
-    studentcourses.add_student_course("Abida", ["Databases", 100, 200])
-    # Validate entry was added
-    res1 = studentcourses.get_student_courses("Abida")
-    # Delete (Abida, Databases) entry
-    studentcourses.delete_student_course("Abida", "Databases")
-    # Validate entry was deleted
-    res2 = studentcourses.get_student_courses("Abida")
-    # Add overlapping OS course
-    studentcourses.add_student_course("Abida", ["Databases", 100, 200])
-    studentcourses.add_student_course("Abida", ["OS", 150, 250])
-    # Validate only DB was added
-    res3 = studentcourses.get_student_courses("Abida")
-    # Both Databases and OS should have been added
-    studentcourses.add_student_course("Abida", ["OS", 250, 260])
-    res4 = studentcourses.get_student_courses("Abida")
-    print(res1, res2, res3, res4)
+    # studentcourses.add_student_course("Abida", ["Databases", 100, 200])
+    # # Validate entry was added
+    # res1 = studentcourses.get_student_courses("Abida")
+    # # Delete (Abida, Databases) entry
+    # studentcourses.delete_student_course("Abida", "Databases")
+    # # Validate entry was deleted
+    # res2 = studentcourses.get_student_courses("Abida")
+    # # Add overlapping OS course
+    # studentcourses.add_student_course("Abida", ["Databases", 100, 200])
+    # # studentcourses.add_student_course("Abida", ["OS", 150, 250])
+    # # Validate only DB was added
+    # res3 = studentcourses.get_student_courses("Abida")
+    # # Both Databases and OS should have been added
+    # studentcourses.add_student_course("Abida", ["OS", 250, 260])
+    # res4 = studentcourses.get_student_courses("Abida")
+    # res5 = studentcourses.print_table()
+    # print(res, res1, res2, res3, res4, res5)
+    studentcourses.close_connection()
 
 if __name__ == "__main__":
     main()
